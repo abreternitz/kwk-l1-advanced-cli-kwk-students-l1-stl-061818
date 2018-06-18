7bf7147355b2d2012abb3c08120e34d7c15ac240
abreternitz <aliceburian@gmail.com>
2018-06-18 19:16:59 +0000

    automatically backed up by learn

---
+++ stock_supermarket.sh
@@ -0,0 +1,10 @@
+ls;
+mv delivery/milk.txt dairy/milk.txt
+mv delivery/icecream.txt dairy/icecream.txt
+mv delivery/velveeta_cheese.txt dairy/velveeta_cheese.txt
+mv delivery/brie.txt dairy/brie.txt
+mv delivery/fage_yogurt.txt dairy/fage_yogurt.txt
+mv delivery/land_o_lakes_butter.txt dairy/land_o_lakes_butter.txt
+
+mv delivery/
+ls
