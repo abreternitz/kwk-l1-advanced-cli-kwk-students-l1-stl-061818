bfe8ad9d1001762277422edff3c2c160faf74ea9
abreternitz <aliceburian@gmail.com>
2018-06-18 19:21:59 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -1,10 +1,22 @@
 ls;
-mv delivery/milk.txt dairy/milk.txt
-mv delivery/icecream.txt dairy/icecream.txt
-mv delivery/velveeta_cheese.txt dairy/velveeta_cheese.txt
-mv delivery/brie.txt dairy/brie.txt
-mv delivery/fage_yogurt.txt dairy/fage_yogurt.txt
-mv delivery/land_o_lakes_butter.txt dairy/land_o_lakes_butter.txt
+//dairy
+  mv delivery/milk.txt dairy/milk.txt
+  mv delivery/icecream.txt dairy/icecream.txt
+  mv delivery/velveeta_cheese.txt dairy/velveeta_cheese.txt
+  mv delivery/brie.txt dairy/brie.txt
+  mv delivery/fage_yogurt.txt dairy/fage_yogurt.txt
+  mv delivery/land_o_lakes_butter.txt dairy/land_o_lakes_butter.txt
 
-mv delivery/
+//dessert
+  mv delivery/carrot_cake.txt dessert/carrot_cake.txt
+  mv delivery/tiramisu_cake.txt dessert/tiramisu_cake.txt 
+  mv delivery/flourless_cake.txt dessert/flourless_cake.txt
+
+//freezer
+  mv delivery/eggo_waffles.txt freezer/eggo_waffles.txt
+  mv
+  
+//fruit
+  mv delivery/apples.txt
+mv delivery
 ls
