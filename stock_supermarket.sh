9657dc42a0dbebe72aaf15509b0e275b5295e876
abreternitz <aliceburian@gmail.com>
2018-06-18 20:08:42 +0000

    automatically backed up by learn

--- stock_supermarket.sh
+++ stock_supermarket.sh
@@ -1,5 +1,5 @@
 ls;
-//dairy
+
   mv delivery/milk.txt dairy/milk.txt
   mv delivery/icecream.txt dairy/icecream.txt
   mv delivery/velveeta_cheese.txt dairy/velveeta_cheese.txt
@@ -7,37 +7,32 @@ ls;
   mv delivery/fage_yogurt.txt dairy/fage_yogurt.txt
   mv delivery/land_o_lakes_butter.txt dairy/land_o_lakes_butter.txt
 
-//dessert
   mv delivery/carrot_cake.txt dessert/carrot_cake.txt
   mv delivery/tiramisu_cake.txt dessert/tiramisu_cake.txt 
   mv delivery/flourless_cake.txt dessert/flourless_cake.txt
+  mv delivery/wonder_bread.txt dessert/wonder_bread.txt
 
-//freezer
+
   mv delivery/eggo_waffles.txt freezer/eggo_waffles.txt
-  mv
-  
-//fruit
+
   mv delivery/apples.txt fruit/apples.txt
   mv delivery/grapples.txt fruit/grapples.txt
-  mv delivery/
-  
-//meat
+  mv delivery/watermelon.txt fruit/watermelon.txt
+
   mv delivery/bison_burgers.txt meat/bison_burgers.txt
   mv delivery/hotdogs.txt meat/hotdogs.txt
   mv delivery/lamb_ribs.txt meat/lamb_ribs.txt
   mv delivery/lean_cusine.txt meat/lean_cusine.txt
   mv delivery/salmon_steak.txt meat/salmon_steak.txt
   mv delivery/turkey_breast.txt meat/turkey_breast
-  
-//snax
+  mv delivery/whole_chicken.txt meat/whole_chicken.txt
+
   mv delivery/fig_bars.txt snax/fig_bars.txt
   mv delivery/goldfish.txt snax/goldfish.txt
   mv delivery/mint_milano_cookies.txt snax/mint_milano_cookies.txt
   mv delivery/oreos.txt snax/oreos.txt
   mv delivery/trail_mix.txt snax/trail_mix.txt
-  mv delivery/
 
-//veggies
   mv delivery/broccoli.txt veggies/broccoli.txt
   mv delivery/carrots.txt veggies/carrots.txt
   mv delivery/garlic.txt veggies/garlic.txt
