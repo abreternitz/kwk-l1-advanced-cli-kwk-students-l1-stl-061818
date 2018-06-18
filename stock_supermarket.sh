31eec99fc0fddc9c3888b77ec8118a220e5375cc
abreternitz <aliceburian@gmail.com>
2018-06-18 20:30:22 +0000

    Done.

---
+++ stock_supermarket.sh
@@ -0,0 +1,53 @@
+ls;
+
+  mv delivery/milk.txt dairy/milk.txt
+  mv delivery/icecream.txt dairy/icecream.txt
+  mv delivery/velveeta_cheese.txt dairy/velveeta_cheese.txt
+  mv delivery/brie.txt dairy/brie.txt
+  mv delivery/fage_yogurt.txt dairy/fage_yogurt.txt
+  mv delivery/land_o_lakes_butter.txt dairy/land_o_lakes_butter.txt
+
+  mv delivery/carrot_cake.txt dessert/carrot_cake.txt
+  mv delivery/tiramisu_cake.txt dessert/tiramisu_cake.txt 
+  mv delivery/flourless_cake.txt dessert/flourless_cake.txt
+  mv delivery/wonder_bread.txt dessert/wonder_bread.txt
+
+
+  mv delivery/eggo_waffles.txt freezer/eggo_waffles.txt
+
+  mv delivery/apples.txt fruit/apples.txt
+  mv delivery/grapples.txt fruit/grapples.txt
+  mv delivery/watermelon.txt fruit/watermelon.txt
+
+  mv delivery/bison_burgers.txt meat/bison_burgers.txt
+  mv delivery/hotdogs.txt meat/hotdogs.txt
+  mv delivery/lamb_ribs.txt meat/lamb_ribs.txt
+  mv delivery/lean_cusine.txt meat/lean_cusine.txt
+  mv delivery/salmon_steak.txt meat/salmon_steak.txt
+  mv delivery/turkey_breast.txt meat/turkey_breast
+  mv delivery/whole_chicken.txt meat/whole_chicken.txt
+
+  mv delivery/fig_bars.txt snax/fig_bars.txt
+  mv delivery/goldfish.txt snax/goldfish.txt
+  mv delivery/mint_milano_cookies.txt snax/mint_milano_cookies.txt
+  mv delivery/oreos.txt snax/oreos.txt
+  mv delivery/trail_mix.txt snax/trail_mix.txt
+
+  mv delivery/broccoli.txt veggies/broccoli.txt
+  mv delivery/carrots.txt veggies/carrots.txt
+  mv delivery/garlic.txt veggies/garlic.txt
+  mv delivery/leeks.txt veggies/leeks.txt
+  mv delivery/onion.txt veggies/onion.txt
+  mv delivery/potatoes.txt veggies/potatoes.txt
+  mv delivery/tomatoes.txt veggies/tomatoes.txt
+  
+  grep -r Winston
+  grep -r gorp
+  grep -r gluten
+  grep -r grapes
+  grep -r vampire
+  grep -r canteloupe
+  grep -r youtube
+  grep -r sprinkles
+
+ls
